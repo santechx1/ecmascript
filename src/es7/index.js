d3f8014f644766b3d9bd5fe7ac628b1612a106cf
santechx <santiagogf11@gmail.com>
2020-06-05 12:53:24 -0500

    es7 review

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+/* Includes*/
+// Método de array para buscar un determinado elemento / Array method to search for an specific element
+// es7
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+if (numbers.includes(9)) {
+    console.log('Found!');
+} else {
+    console.log('Not found!');
+}
+
+/*Exponential / Elevar a una potencia */
+// es7
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+console.log(result);
